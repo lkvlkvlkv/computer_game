4733f245c080889733f44df5b74966fbedf82b0a
LKV <a23202418@gmail.com>
2024-05-18 01:53:11 +0800

    feat: implement AlphaBetaSearch, NegaMax_VALUE, NegaScoutSearch

--- hw3/EWN6x7/Search/search.cpp
+++ hw3/EWN6x7/Search/search.cpp
@@ -3,6 +3,9 @@
 #include "../Display/display.h"
 using namespace std;
 
+const int MAX = 1e7;
+const int MIN = -1e7;
+
 Search::Search() {
     depth = 6;
 }
@@ -11,15 +14,15 @@ Search::Search(int depth) {
     this->depth = depth;
 }
 
-int Search::getMove(vector<vector<int>> &board, vector<vector<int>> &position, int color, int turn, int remain[2], ofstream *file) {
+int Search::getMove(vector<vector<int>> board, vector<vector<int>> position, int color, int turn, int remain[2], ofstream *file) {
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     Game game = Game();
     game.fromBoard(board, position, turn, remain);
-    int computerMove = alphaBetaPruning(game, depth, INT_MIN, INT_MAX, color)[1];
-    printf("Computer move: %d\n", computerMove);
+    auto [score, computerMove] = NegaScoutSearch(game, depth, color, MIN, MAX);
+    // compare(game, depth, color);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     cout << "Time taken: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << endl;
-    if (file != nullptr) {
+    if (file != nullptr) { 
         (*file) << depth << " " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << endl;
     }
     return computerMove;
@@ -32,7 +35,7 @@ int Search::redScore(Game &game) {
             int x = game.position[RED][i] / game.m;
             int y = game.position[RED][i] % game.m;
             score += (i + 1) * (x + y);
-            score += (i + 1) * 50;
+            score += (i + 1) * 10;
         }
     }
     return score;
@@ -45,35 +48,98 @@ int Search::blueScore(Game &game) {
             int x = game.position[BLUE][i] / game.m;
             int y = game.position[BLUE][i] % game.m;
             score += (i + 1) * ((game.n - 1 - x) + (game.m - 1 - y));
-            score += (i + 1) * 50;
+            score += (i + 1) * 10;
         }
     }
     return score;
 }
 
-int Search::evaluate(Game &game, int color) {
+int Search::evaluate(Game &game) {
     if (game.isEnd()) {
         int winner = game.winner();
-        if (winner == RED) {
-            return INT_MAX;
+        if (winner == RED_WIN) {
+            return MAX;
         }
-        else if (winner == BLUE) {
-            return INT_MIN;
+        else if (winner == BLUE_WIN) {
+            return MIN;
         }
         else {
-            return 0;
+            return DRAW;
         }
     }
     return redScore(game) - blueScore(game);
 }
 
+array<int, 2> Search::AlphaBetaSearch(Game &game, int depth, int color) {
+    if (color == RED) {
+        auto [score, computerMove] = MAX_VALUE(game, depth, color, MIN, MAX);
+        return {score, computerMove};
+    }
+    auto [score, computerMove] = MIN_VALUE(game, depth, color, MIN, MAX);
+    return {score, computerMove};
+}
+
+array<int, 2> Search::MAX_VALUE(Game &game, int depth, int color, int alpha, int beta) {
+    if (depth == 0 || game.isEnd()) {
+        return {evaluate(game), -1};
+    }
+    vector<array<int, 2>> moves = game.expand(color);
+    if (moves.size() == 0) {
+        return {MIN_VALUE(game, depth - 1, color ^ 1, alpha, beta)[0], -1};
+    }
+    int v = MIN;
+    int best = 0;
+    for (int i = moves.size() - 1; i >= 0; i--) {
+        auto [id, dir] = moves[i];
+        int original = game.move(id, dir, color);
+        int result = MIN_VALUE(game, depth - 1, color ^ 1, alpha, beta)[0];
+        game.undo(id, dir, color, original);
+        if (result > v) {
+            v = result;
+            best = i;
+        }
+        if (v >= beta) {
+            return {v, best};
+        }
+        alpha = max(alpha, v);
+    }
+    return {v, best};
+}
+
+array<int, 2> Search::MIN_VALUE(Game &game, int depth, int color, int alpha, int beta) {
+    if (depth == 0 || game.isEnd()) {
+        return {evaluate(game), -1};
+    }
+    vector<array<int, 2>> moves = game.expand(color);
+    if (moves.size() == 0) {
+        return {MAX_VALUE(game, depth - 1, color ^ 1, alpha, beta)[0], -1};
+    }
+    int v = MAX;
+    int best = 0;
+    for (int i = moves.size() - 1; i >= 0; i--) {
+        auto [id, dir] = moves[i];
+        int original = game.move(id, dir, color);
+        int result = MAX_VALUE(game, depth - 1, color ^ 1, alpha, beta)[0];
+        game.undo(id, dir, color, original);
+        if (result < v) {
+            v = result;
+            best = i;
+        }
+        if (v <= alpha) {
+            return {v, best};
+        }
+        beta = min(beta, v);
+    }
+    return {v, best};
+}
+
 array<int, 2> Search::alphaBetaPruning(Game &game, int depth, int alpha, int beta, int color) {
     if (depth == 0 || game.isEnd()) {
-        return {evaluate(game, color), -1};
+        return {evaluate(game), -1};
     }
     vector<array<int, 2>> moves = game.expand(color);
     if (color == 0) {
-        int v = INT_MIN;
+        int v = MIN;
         int best = 0;
         if (moves.size() == 0) {
             return {alphaBetaPruning(game, depth - 1, alpha, beta, 1)[0], -1};
@@ -95,7 +161,7 @@ array<int, 2> Search::alphaBetaPruning(Game &game, int depth, int alpha, int bet
         return {v, best};
     }
     else {
-        int v = INT_MAX;
+        int v = MAX;
         int best = 0;
         if (moves.size() == 0) {
             return {alphaBetaPruning(game, depth - 1, alpha, beta, 0)[0], -1};
@@ -116,4 +182,78 @@ array<int, 2> Search::alphaBetaPruning(Game &game, int depth, int alpha, int bet
         }
         return {v, best};
     }
+}
+
+array<int, 2> Search::NegaMax_VALUE(Game &game, int depth, int color, int alpha, int beta) {
+    if (depth == 0 || game.isEnd()) {
+        return {(color == 0 ? 1 : -1) *evaluate(game), -1};
+    }
+    vector<array<int, 2>> moves = game.expand(color);
+    if (moves.size() == 0) {
+        return {-NegaMax_VALUE(game, depth - 1, color ^ 1, -beta, -alpha)[0], -1};
+    }
+    int v = MIN;
+    int best = 0;
+    for (int i = moves.size() - 1; i >= 0; i--) {
+        auto [id, dir] = moves[i];
+        int original = game.move(id, dir, color);
+        int result = -NegaMax_VALUE(game, depth - 1, color ^ 1, -beta, -alpha)[0];
+        game.undo(id, dir, color, original);
+        if (result > v) {
+            v = result;
+            best = i;
+        }
+        if (v >= beta) {
+            return {v, best};
+        }
+        alpha = max(alpha, v);
+    }
+    return {v, best};
+}
+
+array<int, 2> Search::NegaScoutSearch(Game &game, int depth, int color, int alpha, int beta) {
+    if (depth == 0 || game.isEnd()) {
+        return {(color == 0 ? 1 : -1) * evaluate(game), -1};
+    }
+    vector<array<int, 2>> moves = game.expand(color);
+    if (moves.size() == 0) {
+        return {-NegaScoutSearch(game, depth - 1, color ^ 1, -beta, -alpha)[0], -1};
+    }
+    bool fFoundPV = false;
+    int best = 0;
+    for (int i = moves.size() - 1; i >= 0; i--) {
+        auto [id, dir] = moves[i];
+        int original = game.move(id, dir, color);
+        int result;
+        if (fFoundPV) {
+            result = -NegaScoutSearch(game, depth - 1, color ^ 1, -alpha - 1, -alpha)[0];
+            if (result > alpha && result < beta) {
+                result = -NegaScoutSearch(game, depth - 1, color ^ 1, -beta, -alpha)[0];
+            }
+        }
+        else {
+            result = -NegaScoutSearch(game, depth - 1, color ^ 1, -beta, -alpha)[0];
+        }
+        game.undo(id, dir, color, original);
+        if (result > alpha) {
+            alpha = result;
+            fFoundPV = true;
+            best = i;
+        }
+        if (result >= beta) {
+            return {beta, best};
+        }
+    }
+    return {alpha, best};
+}
+
+void Search::compare(Game &game, int depth, int color) {
+    auto [score1, computerMove1] = AlphaBetaSearch(game, depth, color);
+    cout << setw(20) << "AlphaBetaSearch:" << "    Score: " << score1 << " Computer move: " << computerMove1 << endl;
+    auto [score2, computerMove2] = NegaMax_VALUE(game, depth, color, MIN, MAX);
+    cout << setw(20) << "NegaMax_VALUE:" << "    Score: " << score2 << " Computer move: " << computerMove2 << endl;
+    auto [score3, computerMove3] = NegaScoutSearch(game, depth, color, MIN, MAX);
+    cout << setw(20) << "NegaScoutSearch:" << "    Score: " << score3 << " Computer move: " << computerMove3 << endl;
+    auto [score4, computerMove4] = alphaBetaPruning(game, depth, MIN, MAX, color);
+    cout << setw(20) << "alphaBetaPruning:" << "    Score: " << score4 << " Computer move: " << computerMove4 << endl;
 }
--- hw3/EWN6x7/Search/search.h
+++ hw3/EWN6x7/Search/search.h
@@ -14,15 +14,23 @@ public:
 
     Search(int depth);
 
-    int getMove(vector<vector<int>> &board, vector<vector<int>> &position, int color, int turn, int remain[2], ofstream *file);
+    int getMove(vector<vector<int>> board, vector<vector<int>> position, int color, int turn, int remain[2], ofstream *file);
 
     int redScore(Game &game);
 
     int blueScore(Game &game);
 
-    int evaluate(Game &game, int color);
+    int evaluate(Game &game);
+
+    void compare(Game &game, int depth, int color);
 
     array<int, 2> alphaBetaPruning(Game &game, int depth, int alpha, int beta, int color);
+    array<int, 2> AlphaBetaSearch(Game &game, int depth, int color);
+    array<int,2> MAX_VALUE(Game &game, int depth, int color, int alpha, int beta);
+    array<int,2> MIN_VALUE(Game &game, int depth, int color, int alpha, int beta);
+    array<int, 2> NegaMax_VALUE(Game &game, int depth, int color, int alpha, int beta);
+    array<int, 2> NegaScoutSearch(Game &game, int depth, int color, int alpha, int beta);
+
 };
 
 #endif
